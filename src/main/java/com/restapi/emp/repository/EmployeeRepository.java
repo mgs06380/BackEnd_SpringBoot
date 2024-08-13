77c626f6a899e8b34b065bc2c030b613def32bb1
mgs06380 <mgs06380@naver.com>
2024-08-13 10:56:17 +0900

    total

--- src/main/java/com/restapi/emp/repository/EmployeeRepository.java
+++ src/main/java/com/restapi/emp/repository/EmployeeRepository.java
@@ -7,8 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    // Query method, Finder method
+    //Query method, Finder method
     Optional<Employee> findByEmail(String email);
     List<Employee> findByFirstNameContains(String firstName);
-
 }
--- src/main/java/com/restapi/emp/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/restapi/emp/service/impl/DepartmentServiceImpl.java
@@ -6,15 +6,16 @@ import com.restapi.emp.entity.Department;
 import com.restapi.emp.exception.ResourceNotFoundException;
 import com.restapi.emp.repository.DepartmentRepository;
 import com.restapi.emp.service.DepartmentService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class DepartmentServiceImpl implements DepartmentService {
     private final DepartmentRepository departmentRepository;
@@ -31,52 +32,57 @@ public class DepartmentServiceImpl implements DepartmentService {
         return DepartmentMapper.mapToDepartmentDto(savedDepartment);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public DepartmentDto getDepartmentById(Long departmentId) {
 //        Optional<Department> optional = departmentRepository.findById(departmentId);
 //        Department department = optional.orElseThrow(
 //                () -> new ResourceNotFoundException("Department is not exists with a given id: " + departmentId) );
 
-        String errMsg = String.format("Department is not exists with a given id: %s",departmentId);
-        Department department = departmentRepository.findById(departmentId)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException(errMsg, HttpStatus.NOT_FOUND)
-                );
+        Department department = EmpDeptCommon.getDepartment(departmentId, departmentRepository);
         return DepartmentMapper.mapToDepartmentDto(department);
     }
+    
+// EmpDeptCommon 클래스의 getDepartment() 메서드로 대체됨
+//    private Department getDepartment(Long departmentId) {
+//        String errMsg = String.format("Department is not exists with a given id: %s", departmentId);
+//        return departmentRepository.findById(departmentId)
+//                .orElseThrow(() ->
+//                        new ResourceNotFoundException(errMsg, HttpStatus.NOT_FOUND)
+//        );
+//    }
 
+    @Transactional(readOnly = true)
     @Override
     public List<DepartmentDto> getAllDepartments() {
+        // List<Department> ==> List<DepartmentDto>
         List<Department> departments = departmentRepository.findAll();
-        return departments.stream()
+        return departments
+                .stream()
                 .map(DepartmentMapper::mapToDepartmentDto)
-                .toList();
-        //.map((department) -> DepartmentMapper.mapToDepartmentDto(department))
-        //.collect(Collectors.toList());
+                .toList(); //Stream<DepartmentDto> => List<DepartmentDto>
+                //.map((department) -> DepartmentMapper.mapToDepartmentDto(department))
+                //.collect(Collectors.toList());
     }
 
     @Override
     public DepartmentDto updateDepartment(Long departmentId, DepartmentDto updatedDepartment) {
-        Department department = departmentRepository.findById(departmentId)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Department is not exists with a given id:"+ departmentId)
-                );
+        Department department = EmpDeptCommon.getDepartment(departmentId, departmentRepository);
+        //Dirty Check - setter method 호출
+        if (updatedDepartment.getDepartmentName() != null)
+            department.setDepartmentName(updatedDepartment.getDepartmentName());
+        if (updatedDepartment.getDepartmentDescription() != null)
+            department.setDepartmentDescription(updatedDepartment.getDepartmentDescription());
 
-        department.setDepartmentName(updatedDepartment.getDepartmentName());
-        department.setDepartmentDescription(updatedDepartment.getDepartmentDescription());
+        //Department savedDepartment = departmentRepository.save(department);
 
-        Department savedDepartment = departmentRepository.save(department);
-
-        return DepartmentMapper.mapToDepartmentDto(savedDepartment);
+        return DepartmentMapper.mapToDepartmentDto(department);
     }
 
     @Override
     public void deleteDepartment(Long departmentId) {
-        departmentRepository.findById(departmentId)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Department is not exists with a given id: " + departmentId)
-                );
+        Department department = EmpDeptCommon.getDepartment(departmentId, departmentRepository);
 
-        departmentRepository.deleteById(departmentId);
+        departmentRepository.delete(department);
     }
 }
